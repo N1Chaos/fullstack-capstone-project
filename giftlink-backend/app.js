6df570b55e164542bddef476d097d3e73f0f1be9
N1Chaos <sagiadinos@wanadoo.fr>
2024-07-06 05:16:09 -0400

    correction 06072024

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,7 +23,6 @@ app.use(express.json());
 
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
-const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -32,7 +31,6 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
-app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
 
 // Global Error Handler
@@ -42,8 +40,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server");
-});
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
