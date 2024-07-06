266eec65aa452d2eabc36452d7b9766ec89db435
N1Chaos <sagiadinos@wanadoo.fr>
2024-07-06 05:58:27 -0400

    correction bis 06072024

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -3,13 +3,11 @@ import React, { useState } from 'react';
 import './RegisterPage.css';
 
 function RegisterPage() {
-    //insert code here to create useState hook variables for firstName, lastName, email, password
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    // insert code here to create handleRegister function and include console.log
     const handleRegister = async () => {
         console.log("Register invoked")
     }
@@ -20,8 +18,6 @@ return (
             <div className="col-md-6 col-lg-4">
                 <div className="register-card p-4 border rounded">
                     <h2 className="text-center mb-4 font-weight-bold">Register</h2>
-                    
-                    {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
                     <div className="mb-3">
                         <label htmlFor="firstName" className="form-label">FirstName</label>
                         <input
@@ -72,8 +68,6 @@ return (
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-
-                    {/* insert code here to create a button that performs the `handleRegister` function on click */}
                     <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
                     <p className="mt-4 text-center">
                         Already a member? <a href="/app/login" className="text-primary">Login</a>
